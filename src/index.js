92f47fa019e955d21b8a14b5cedc2cc7fb7005a4
youcef idiri <iyucef@gmail.com>
2021-09-29 13:24:56 +0100

    fix: use-session-storage-state export

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { useLocalStorageState } from './useLocalStorageState'
 export { useSafeDispatch } from './useSafeDispatch'
 export { useAsync } from './useAsync'
+export { useSessionStorageState } from './useSessionStorageState'
